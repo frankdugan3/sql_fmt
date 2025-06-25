40611aa7a3f1f6ab358ddb50ee077a05e1de1418
Frank Dugan III <frank3@duganator.com>
2025-06-25 12:19:07 -0500

    Enable use in Ecto fragments

--- test/sql_fmt/helpers_test.exs
+++ test/sql_fmt/helpers_test.exs
@@ -1,6 +1,7 @@
 defmodule SqlFmt.HelpersTest do
   use ExUnit.Case
   import SqlFmt.Helpers
+  import Ecto.Query
 
   test "sigil_SQL" do
     assert ~SQL"SELECT *" == "SELECT *"
@@ -14,4 +15,7 @@ defmodule SqlFmt.HelpersTest do
              FROM users
              """
   end
+
+  assert from("my_table", select: fragment(~SQL"? > ?", 2, 1))
+  assert from("my_table", select: fragment(~SQL"now()"))
 end
